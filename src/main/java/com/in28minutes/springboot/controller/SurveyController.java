264ce4ba0cbd5c93aaf1619d3c71c22baa07822d
Yaser <yaser.ghaderipoor@gmail.com>
2020-03-03 17:09:19 +0330

    Spring Boot Deep Dive with a simple API: step 10 -Add POST api to create a new resource and return its uri with "Created" status(201)

--- src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -3,10 +3,11 @@ package com.in28minutes.springboot.controller;
 import com.in28minutes.springboot.model.Question;
 import com.in28minutes.springboot.service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,9 +20,19 @@ public class SurveyController {
         return surveyService.retrieveQuestions(surveyId);
     }
 
+    @PostMapping("/surveys/{surveyId}/questions")
+    public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId,
+                                              @RequestBody Question newQuestion) {
+        Question question = surveyService.addQuestion(surveyId, newQuestion);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(question.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
     @GetMapping("/surveys/{surveyId}/questions/{questionId}")
     public Question retrieveDetailsForQuestion(@PathVariable String surveyId,
-                                                     @PathVariable String questionId) {
+                                               @PathVariable String questionId) {
         return surveyService.retrieveQuestion(surveyId, questionId);
     }
 }
