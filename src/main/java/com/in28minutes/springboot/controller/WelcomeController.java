1194ffc20e3b75bf5020117be607c0095ca95a6b
Yaser <yaser.ghaderipoor@gmail.com>
2020-03-06 11:02:36 +0330

    Spring Boot Deep Dive with a simple API: step 15 -Use properties value in the controller and overwrite it in the program argument.

--- src/main/java/com/in28minutes/springboot/controller/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/controller/WelcomeController.java
@@ -1,13 +1,26 @@
 package com.in28minutes.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    @Value("${welcome.message}")
+    private String message;
+
+    @Value("${welcome2.message}")
+    private String message2;
+
     @RequestMapping("/welcome")
     public String welcome() {
-        return "This is great! I can see this on web page.";
+        return message;
+    }
+
+    @GetMapping("/welcome2")
+    public String goodbay() {
+        return message2;
     }
 }
