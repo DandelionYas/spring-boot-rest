dfc95a82c8238ecb51c97c90ba80739daf00bcb0
Yaser <yaser.ghaderipoor@gmail.com>
2020-03-07 21:41:31 +0330

    Spring Boot Deep Dive with a simple API: step 20 -Add spring boot data rest dependency -Publish UserRepository as a rest resource

--- src/main/java/com/in28minutes/springboot/repository/UserRepository.java
+++ src/main/java/com/in28minutes/springboot/repository/UserRepository.java
@@ -2,9 +2,12 @@ package com.in28minutes.springboot.repository;
 
 import com.in28minutes.springboot.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "users", collectionResourceRel = "users")
 public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findByRole(String role);
+    List<User> findByRole(@Param("role") String role);// /users/search/findByRole?role=Admin
 }
