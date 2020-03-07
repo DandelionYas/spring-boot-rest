ed0c162740fb0dda967cb86f42320b5061960596
Yaser <yaser.ghaderipoor@gmail.com>
2020-03-07 13:34:51 +0330

    Spring Boot Deep Dive with a simple API: step 17 -Use @ConfigurationProperties -Understand its benefits which is Type Safe, Organizes the all of the properties values in one class and prevents using a lot of @Value in the project

--- src/main/java/com/in28minutes/springboot/controller/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/controller/WelcomeController.java
@@ -1,9 +1,14 @@
 package com.in28minutes.springboot.controller;
 
+import com.in28minutes.springboot.configuration.BasicConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
@@ -14,6 +19,9 @@ public class WelcomeController {
     @Value("${welcome2.message}")
     private String message2;
 
+    @Autowired
+    private BasicConfiguration basicConfiguration;
+
     @RequestMapping("/welcome")
     public String welcome() {
         return message;
@@ -22,5 +30,14 @@ public class WelcomeController {
     @GetMapping("/welcome2")
     public String goodbay() {
         return message2;
+    }
+
+    @GetMapping("/dynamic-configuration")
+    private Map dynamicConfiguration() {
+        Map map = new HashMap();
+        map.put("message",basicConfiguration.getMessage());
+        map.put("number", basicConfiguration.getNumber());
+        map.put("value", basicConfiguration.isValue());
+        return map;
     }
 }
